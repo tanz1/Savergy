6e44b0168db1d7dda485e69bc26ed4697c5f7e9d
MSI\Mike <mik3tanzi@gmail.com>
2018-03-07 22:33:42 -0500

    Working Prototype
    This change modifies the menu icon size and adds a functional javascript that creates the first working prototype.

--- calc.js
+++ calc.js
@@ -1,3 +1,21 @@
+function calculate()
+{
+    var i = 1;
+    var total = [];
+ 
+    while(i < 6)
+    {
+        var electronic = document.getElementById('appliance' +i ).value;
+        var quantity = document.getElementById('quantity' +i).value;
+        var hours = document.getElementById('hours' +i).value;
+        total[i-1] = electronic * quantity * hours;
+        i++;
+    }
+       document.getElementById("output").innerHTML = total.reduce(function(a, b){return a+b;});
+       // assistance from https://stackoverflow.com/questions/16057672/sum-values-from-an-array-in-javascript
+}
+
+// source url for hamburger menu https://github.com/mranenko/hamburger-menu
 (function() {
 
 	var hamburger = {
@@ -14,4 +32,10 @@
 	hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
 
 }());
-// source url for hamburger menu https://github.com/mranenko/hamburger-menu
+
+
+
+
+
+
+
